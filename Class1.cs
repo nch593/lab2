bef59688de7613a4dfe04ca30bfb00ad6c8ba1fa
nch593 <158087957+nch593@users.noreply.github.com>
2024-02-04 23:22:46 -0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace MyTaskLibraryProject
+{
+    public class Class1
+    {
+        public static int FindMin(int number1, int number2)
+        {
+            return Math.Min(number1, number2);
+        }
+
+        public static int FindMin(int number1, int number2, int number3)
+        {
+            return Math.Min(Math.Min(number1, number2), number3);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MyTaskLibraryProject;
+
+namespace TestMyTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1;
+            int number2;
+            int number3;
+
+            Console.Write("Please enter number 1\n");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please enter number 2\n");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please enter number 3\n");
+            number3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The Minimum of 2 values: ", MyTaskLibraryProject.Class1.FindMin(number1, number2));
+            Console.WriteLine("The Minimum of 3 values: ", MyTaskLibraryProject.Class1.FindMin(number1, number2, number3));
+
+        }
+    }
+}
